d946f9024a73f6958fa7f0f47b7f18f32d9e0857
Mitchell Grams <mitchell.grams@apollomachine.com>
2026-01-13 13:08:28 -0700

    adjust purge delay

--- fans.c
+++ fans.c
@@ -263,7 +263,7 @@ static void userMCodeExecute (uint_fast16_t state, parser_block_t *gc_block)
             ldm_set_state(Hopper1Enable, Off);
             ldm_set_state(Hopper2Enable, Off);
             
-            delay_sec(1.0f, DelayMode_Dwell);
+            delay_sec(0.1f, DelayMode_Dwell);
             ldm_set_state(NozzlePurge, !ldm_get_state(NozzlePurge));
 
             // delay_sec(10.f, DelayMode_Dwell);
